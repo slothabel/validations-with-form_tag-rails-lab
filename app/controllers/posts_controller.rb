6732c996bafc93adc2945f2f80f639eafab6b88c
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-09-28 19:51:34 -0400

    commit tree 481741b77be6072688259d08c54b1c59d096568c

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    if @post.valid?
-      @post.save
+    if @post.save
       redirect_to post_path(@post)
     else
       render :new
