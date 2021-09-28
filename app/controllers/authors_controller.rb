4f88a215bf7e19586c217df2a98e67aaf7960186
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-09-28 19:46:40 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,19 +1,40 @@
 class AuthorsController < ApplicationController
   def show
-    @author = Author.find(params[:id])
+    find_author
   end
 
   def new
     @author = Author.new
   end
 
-  def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+  def edit
+    find_author
   end
 
+  def create
+    @author = Author.create(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @author.update(author_params)
+    if @author.valid?
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
+  end
+  
   private
+
+  def find_author
+    @author = Author.find(params[:id])
+  end
 
   def author_params
     params.permit(:name, :email, :phone_number)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,39 @@
 class PostsController < ApplicationController
   def show
-    @post = Post.find(params[:id])
+    find_post
   end
 
   def edit
-    @post = Post.find(params[:id])
+    find_post
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    find_post
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.permit(:title, :category, :content)
